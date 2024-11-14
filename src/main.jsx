183de2aba16d2ff2a87e1bd740e55fc8c9ad2858
B2014580_DangThanhLiem <liemb2014580@student.ctu.edu.vn>
2024-11-14 20:22:04 +0700

    Create Footer

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import ReactDom from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {BrowserRouter} from 'react-router-dom'
-import  StoreContextProvider  from './context/storeContext.jsx'
-ReactDom.createRoot(document.getElementById('root')).render(
+import  StoreContextProvider  from './context/StoreContext.jsx'
+ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <StoreContextProvider>
       <App />
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,14 +3,18 @@ import "./Home.css"
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
+import Footer from '../../components/Footer/Footer'
 const Home = () => {
     const [category,setCategory] = useState("All");
   return (
+    <>
     <div >
         <Header/>
         <ExploreMenu category={category} setCategory={setCategory}/>
         <FoodDisplay category={category}/>
     </div>
+    <Footer/>
+    </>
   )
 }
 
